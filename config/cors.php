1804b42217b00091a975f69773247b61c4e83243
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-13 04:08:06 +0800

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -1,28 +1,11 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Laravel CORS Options
-    |--------------------------------------------------------------------------
-    |
-    | You can configure your CORS options here. These options determine the
-    | behavior of CORS requests and what headers are sent to allow them.
-    |
-    */
-
-    'paths' => ['api/*'], // Tentukan path yang memerlukan CORS
-
-    'allowed_methods' => ['*'], // Mengizinkan semua metode HTTP (GET, POST, PUT, DELETE, dll)
-
-    'allowed_origins' => ['*'], // Mengizinkan semua asal (domain). Anda bisa membatasi sesuai kebutuhan
-
-    'allowed_headers' => ['*'], // Mengizinkan semua header
-
-    'exposed_headers' => [], // Menentukan header mana yang harus diekspos
-
-    'max_age' => 0, // Menentukan cache untuk preflight request
-
-    'supports_credentials' => false, // Apakah mendukung kredensial (cookies atau header Authorization)
+    'paths' => ['*'],
+    'allowed_methods' => ['GET, POST, PUT, DELETE'],
+    'allowed_origins' => ['http://localhost:5173'], 
+    'allowed_headers' => ['*'], 
+    'exposed_headers' => [], 
+    'max_age' => 60,
+    'supports_credentials' => false,
 ];
