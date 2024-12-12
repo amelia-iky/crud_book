a60f448b8d8b708f7cdde1ef09a49808cdc4e0ec
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-13 04:07:12 +0800

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
-/** @var \Laravel\Lumen\Routing\Router $router */
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/books', 'BookController@index');
+$router->get('/books/{id}', 'BookController@show');
+$router->post('/books', 'BookController@store');
+$router->put('/books/{id}', 'BookController@update');
+$router->delete('/books/{id}', 'BookController@destroy');
+
+
