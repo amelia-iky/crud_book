12e2b7d7c81924957744375754d13c2eb2d7c4da
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-13 08:10:54 +0800

    update css framework

--- resources/tailwind.config.js
+++ resources/tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{html,js,jsx}"],
+    content: ["./index.html", "./src/**/*.{js,jsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: "#113F67", // Warna utama
+                    light: "#6cb2eb",
+                    dark: "#226597",
+                    custom: "#F3F9FB",
+                },
+                white: "#F3F9FB", // Warna putih
+                default: "#FFFFFF", // Warna default
+            },
+            container: {
+                center: true,
+                padding: {
+                    DEFAULT: "1rem",
+                    sm: "1rem",
+                    lg: "1rem",
+                    xl: "6rem",
+                    "2xl": "6rem",
+                },
+            },
+            fontFamily: {
+                poppins: ["Poppins"],
+                main: ["Poppins"],
+            },
+        },
     },
     plugins: [],
 };
