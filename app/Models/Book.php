d3fea2faa2c2e036223db8457f1635a2bad9a4ab
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-13 22:34:51 +0800

    update id use uuid

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,9 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Book extends Model
 {
     protected $table = 'books';
     protected $fillable = ['title', 'author', 'year'];
+
+    // Primary key
+    public $incrementing = false;
+
+    // Key type
+    protected $keyType = 'string';
+
+    // Set UUID as primary key
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = "book-" . substr((string) Str::uuid(), 0, 5);
+            }
+        });
+    }
 }
