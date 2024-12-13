29e8f1ba9798aeb751f32531ce66128acafc3b31
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-13 22:36:12 +0800

    fix button

--- resources/src/components/Button.jsx
+++ resources/src/components/Button.jsx
@@ -6,12 +6,13 @@ const Button = ({
     variant = "primary",
     children,
 }) => {
-    const baseClass = "py-2 px-4 rounded font-semibold";
+    const baseClass = "py-1 px-3 rounded font-semibold";
 
     const variantClass = {
         added: "bg-green-600 text-white hover:bg-green-700",
         deleted: "bg-red-600 text-white hover:bg-red-700",
         edited: "bg-primary-dark text-white hover:bg-primary",
+        pagination: "bg-gray-600 text-white hover:bg-gray-700",
     };
 
     const resolvedVariantClass = variantClass[variant] || variantClass.primary;
@@ -31,7 +32,7 @@ const Button = ({
 Button.propTypes = {
     type: PropTypes.oneOf(["button", "submit", "reset"]),
     onClick: PropTypes.func,
-    variant: PropTypes.oneOf(["added", "deleted", "edited"]),
+    variant: PropTypes.oneOf(["added", "deleted", "edited", "pagination"]),
     children: PropTypes.node,
 };
 
