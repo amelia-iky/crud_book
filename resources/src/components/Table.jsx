47c6f3c112b699eb0c6d0a219bcd75cdf8c45c48
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-13 15:18:52 +0800

    update

--- resources/src/components/Table.jsx
+++ resources/src/components/Table.jsx
@@ -1,17 +1,16 @@
 import useFetchBook from "../hooks/useFetchBook";
 import useDeleteBook from "../hooks/useDeleteBook";
+import useEditBook from "../hooks/useEditBook";
 import Button from "./Button";
 import BookForm from "./BookForm";
 import { useState } from "react";
 
 const TableBook = () => {
     const { books, setBooks } = useFetchBook();
+    const { editBook } = useEditBook();
     const { deleteBook } = useDeleteBook();
     const [showForm, setShowForm] = useState(false);
-
-    const handleAddClick = () => {
-        setShowForm(true);
-    };
+    const [editingBook, setEditingBook] = useState(null);
 
     return (
         <>
@@ -20,14 +19,21 @@ const TableBook = () => {
             </div>
 
             <div className="justify-center p-4 mx-28">
-                <Button type="button" variant="added" onClick={handleAddClick}>
+                <Button
+                    type="button"
+                    variant="added"
+                    onClick={() => {
+                        setEditingBook(books);
+                        setShowForm(true);
+                    }}
+                >
                     + Tambah
                 </Button>
 
                 <div className="border border-gray-200 rounded-lg shadow-md mt-3">
                     <table className="w-full text-center">
                         <thead>
-                            <tr className="text-gray-500 text-sm border-b">
+                            <tr className="text-sm border-b">
                                 <th className="p-3">ID</th>
                                 <th className="py-3">Judul</th>
                                 <th className="py-3">Penulis</th>
@@ -38,24 +44,33 @@ const TableBook = () => {
                         <tbody>
                             {books && books.length > 0 ? (
                                 books.map((book) => (
-                                    <tr className="text-gray-700" key={book.id}>
+                                    <tr
+                                        className="text-gray-700 border-b"
+                                        key={book.id}
+                                    >
                                         <td className="py-2">{book.id}</td>
                                         <td className="py-2">{book.title}</td>
                                         <td className="py-2">{book.author}</td>
                                         <td className="py-2">{book.year}</td>
                                         <td className="py-2">
                                             <span className="flex gap-2 justify-center">
+                                                {/* Form edit */}
                                                 <Button
                                                     type="button"
                                                     variant="edited"
-                                                    onClick={() =>
-                                                        alert(
-                                                            "Secondary Button Clicked!"
-                                                        )
-                                                    }
+                                                    onClick={() => {
+                                                        editBook(book.id, {
+                                                            title: book.title,
+                                                            author: book.author,
+                                                            year: book.year,
+                                                        });
+                                                        setShowForm(true);
+                                                    }}
                                                 >
                                                     Edit
                                                 </Button>
+
+                                                {/* Form add */}
                                                 <Button
                                                     type="button"
                                                     variant="deleted"
@@ -91,18 +106,33 @@ const TableBook = () => {
                 </div>
 
                 {showForm && (
-                    <>
-                        <div
-                            className="fixed inset-0 bg-gray-900 bg-opacity-50 z-10"
-                            onClick={() => setShowForm(false)}
-                        />
-
+                    <div
+                        className="fixed inset-0 bg-black bg-opacity-50 z-10"
+                        onClick={() => setShowForm(false)}
+                    >
                         <div className="fixed inset-0 flex justify-center items-center z-20">
-                            <div className="bg-white rounded-lg shadow-lg p-6">
-                                <BookForm onClose={() => setShowForm(false)} />
+                            <div
+                                className="bg-white rounded-lg shadow-lg p-6"
+                                onClick={(e) => e.stopPropagation()}
+                            >
+                                <BookForm
+                                    book={editingBook}
+                                    onClose={() => setShowForm(false)}
+                                    onSave={(updatedBook) => {
+                                        editBook(updatedBook.id, updatedBook);
+                                        setBooks((prevBooks) =>
+                                            prevBooks.map((b) =>
+                                                b.id === updatedBook.id
+                                                    ? updatedBook
+                                                    : b
+                                            )
+                                        );
+                                        setShowForm(false);
+                                    }}
+                                />
                             </div>
                         </div>
-                    </>
+                    </div>
                 )}
             </div>
         </>
