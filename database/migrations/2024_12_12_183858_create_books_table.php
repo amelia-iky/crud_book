8d53a7862107f3cf6d27781c43527c37663248d9
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-13 22:35:02 +0800

    use uuid for id

--- database/migrations/2024_12_12_183858_create_books_table.php
+++ database/migrations/2024_12_12_183858_create_books_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('books', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary;
             $table->string('title');
             $table->string('author');
             $table->integer('year');
